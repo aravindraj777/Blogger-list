902b24645bd3d0d553d726f4348292e35de1eb81
Aravind <aravindmunna240@gmail.com>
2023-11-18 20:10:37 +0530

    used-selectors in ngrx

--- src/app/component/customcounter/customcounter.component.ts
+++ src/app/component/customcounter/customcounter.component.ts
@@ -1,20 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
+import { Subscription } from 'rxjs';
 import { customIncrement } from 'src/app/shared/store/counter.actions';
 import { CounterModel } from 'src/app/shared/store/counter.model';
+import { getChannelName } from 'src/app/shared/store/counter.selector';
 
 @Component({
   selector: 'app-customcounter',
   templateUrl: './customcounter.component.html',
   styleUrls: ['./customcounter.component.css']
 })
-export class CustomcounterComponent {
+export class CustomcounterComponent implements OnInit {
+  
+  counterSubscribe!:Subscription;
 
   constructor(private store:Store<{counter:CounterModel}>){
 
   }
+  ngOnInit(): void {
+    this.counterSubscribe =  this.store.select(getChannelName).subscribe((data)=>{
+    
+      this.channelName = data;
+    })
+  }
   counterinput!:number;
   actionType=''
+  channelName=''
 
   onIncrement(){
     this.store.dispatch(customIncrement({value:+this.counterinput,action:this.actionType}))
---
+++ src/app/shared/store/counter.selector.ts
@@ -0,0 +1,15 @@
+import { createFeatureReducerFactory } from "@ngrx/store/src/utils";
+import { CounterModel } from "./counter.model";
+import { createFeatureSelector, createSelector } from "@ngrx/store";
+
+
+const getCounterState = createFeatureSelector<CounterModel>('counter')
+
+
+export const getCounter = createSelector(getCounterState,(state)=>{
+    return state.counter;
+})
+
+export const getChannelName = createSelector(getCounterState,(state)=>{
+    return state.channelName;
+})
